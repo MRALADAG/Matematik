cc12643e24cef103d2df1ee1fc33c56095b1895a
MRALADAG <mstf_aladag@yahoo.com>
2021-01-17 18:27:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(5, 6);
+            // Burada class'ın içersindeki metoda ulaşabilmek için o class'ın türünde bir nesne oluşturduk.
+
+            dortIslem.Topla(21, 9);
+        }
+    }
+}
